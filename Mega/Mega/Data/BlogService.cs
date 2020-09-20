a06d39cf5215812f27d29fcc85b8478bc4df06ba
lybplayer <felix.yang.yicheng@gmail.com>
2020-09-20 18:13:34 +0200

    inject service postservice into startup

---
+++ Mega/Mega/Data/BlogService.cs
@@ -0,0 +1,23 @@
+using Mega.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mega.Data
+{
+    public class BlogService
+    {
+        private readonly BloggingContext _context;
+
+        public  BlogService(BloggingContext context)
+        {
+            _context = context;
+        }
+
+        public List<Blog> GetBlog()
+        {
+            return _context.Blog.ToList() ;
+        }
+    }
+}
---
+++ Mega/Mega/Data/PostService.cs
@@ -0,0 +1,23 @@
+using Mega.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mega.Data
+{
+    public class PostService
+    {
+        private readonly BloggingContext _context;
+
+        public  PostService(BloggingContext context)
+        {
+            _context = context;
+        }
+
+        public List<Post> GetPost()
+        {
+            return _context.Post.ToList() ;
+        }
+    }
+}
